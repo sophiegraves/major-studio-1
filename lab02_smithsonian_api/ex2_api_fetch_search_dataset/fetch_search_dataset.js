811139d24e438b791c4b04f95322bfba9af7f672
sophiegraves <gravs307@newschool.edu>
2024-09-05 14:03:24 -0400

    Update fetch_search_dataset.js

--- lab02_smithsonian_api/ex2_api_fetch_search_dataset/fetch_search_dataset.js
+++ lab02_smithsonian_api/ex2_api_fetch_search_dataset/fetch_search_dataset.js
@@ -3,14 +3,14 @@
 // Using this data set https://collections.si.edu/search/results.htm?q=Flowers&view=grid&fq=data_source%3A%22Cooper+Hewitt%2C+Smithsonian+Design+Museum%22&fq=online_media_type%3A%22Images%22&media.CC0=true&fq=object_type:%22Embroidery+%28visual+works%29%22
 
 // put your API key here;
-const apiKey = "";  
+const apiKey = "jb5mjWwOfmiovVL6pYwcLpXgTFZpgbqvlNoEuLLv";
 
 // search base URL
 const searchBaseURL = "https://api.si.edu/openaccess/api/v1.0/search";
 
 // constructing the initial search query
 // const search =  'mask AND unit_code:"FSG"';
-const search =  `Flowers AND unit_code:"CHNDM" AND object_type:"Embroidery (visual works)" AND online_media_type:"Images"`;
+const search = `Flowers AND unit_code:"CHNDM" AND object_type:"Embroidery (visual works)" AND online_media_type:"Images"`;
 
 
 // array that we will write into
@@ -21,20 +21,20 @@ let jsonString = '';
 
 // search: fetches an array of terms based on term category
 function fetchSearchData(searchTerm) {
-    let url = searchBaseURL + "?api_key=" + apiKey + "&q=" + searchTerm;
-    console.log(url);
-    window
+  let url = searchBaseURL + "?api_key=" + apiKey + "&q=" + searchTerm;
+  console.log(url);
+  window
     .fetch(url)
     .then(res => res.json())
     .then(data => {
       console.log(data)
-      
+
       // constructing search queries to get all the rows of data
       // you can change the page size
       let pageSize = 1000;
       let numberOfQueries = Math.ceil(data.response.rowCount / pageSize);
       console.log(numberOfQueries)
-      for(let i = 0; i < numberOfQueries; i++) {
+      for (let i = 0; i < numberOfQueries; i++) {
         // making sure that our last query calls for the exact number of rows
         if (i == (numberOfQueries - 1)) {
           searchAllURL = url + `&start=${i * pageSize}&rows=${data.response.rowCount - (i * pageSize)}`;
@@ -43,7 +43,7 @@ function fetchSearchData(searchTerm) {
         }
         console.log(searchAllURL)
         fetchAllData(searchAllURL);
-      
+
       }
     })
     .catch(error => {
@@ -54,29 +54,29 @@ function fetchSearchData(searchTerm) {
 // fetching all the data listed under our search and pushing them all into our custom array
 function fetchAllData(url) {
   window
-  .fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
+    .fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
 
-    data.response.rows.forEach(function(n) {
-      addObject(n);
-    });
-    jsonString += JSON.stringify(myArray);
-    console.log(myArray);
-  })
-  .catch(error => {
-    console.log(error)
-  })
+      data.response.rows.forEach(function (n) {
+        addObject(n);
+      });
+      jsonString += JSON.stringify(myArray);
+      console.log(myArray);
+    })
+    .catch(error => {
+      console.log(error)
+    })
 
 }
 
 // create your own array with just the data you need
-function addObject(objectData) {  
-  
+function addObject(objectData) {
+
   // we've encountered that some places have data others don't
   let currentPlace = "";
-  if(objectData.content.indexedStructured.place) {
+  if (objectData.content.indexedStructured.place) {
     currentPlace = objectData.content.indexedStructured.place[0];
   }
 
